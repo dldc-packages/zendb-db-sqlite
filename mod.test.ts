8a0fb69f0575512fd0fd95f83ff26ae22954b687
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2025-10-06 23:55:21 +0200

    Upgrade zendb to v11

--- mod.test.ts
+++ mod.test.ts
@@ -1,22 +1,20 @@
-import { Database as BaseDb } from "@db/sqlite";
-import { Database } from "@dldc/zendb";
+import { Database } from "@db/sqlite";
+import { Utils } from "@dldc/zendb";
 import { expect } from "@std/expect";
-import { DbDatabase } from "./mod.ts";
+import { DbSqliteDriver } from "./mod.ts";
 
 Deno.test("read pragma", () => {
-  const sqldb = new BaseDb(":memory:");
-  const db = DbDatabase(sqldb);
+  const db = new Database(":memory:");
 
-  const res = db.exec(Database.userVersion());
+  const res = DbSqliteDriver.exec(db, Utils.userVersion());
   expect(res).toEqual(0);
 });
 
 Deno.test("write pragma", () => {
-  const sqldb = new BaseDb(":memory:");
-  const db = DbDatabase(sqldb);
+  const db = new Database(":memory:");
 
-  const res = db.exec(Database.setUserVersion(42));
+  const res = DbSqliteDriver.exec(db, Utils.setUserVersion(42));
   expect(res).toEqual(null);
-  const version = db.exec(Database.userVersion());
+  const version = DbSqliteDriver.exec(db, Utils.userVersion());
   expect(version).toEqual(42);
 });
